e9674e47fe5b289cd8241d59a00fae6439dcbb23
David Clyde <dcclyde@gmail.com>
2022-04-12 08:10:08 -0700

    2022-04-10 cf 737d2; solved ABCE in contest and D 1min after; rating 2730, woulda been 2900 if I finished D :O

---
+++ codeforces/2022-04-11_737d2/compare.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+set -e
+
+PROBLEM="C"
+
+g++ --std=c++2a "$PROBLEM".cpp -o a.out \
+    -DDCCLYDE_LOCAL
+g++ --std=c++2a -DDCCLYDE_BRUTEFORCE "$PROBLEM".cpp -o b.out \
+    -DDCCLYDE_LOCAL
+
+for((k = 1; k < 100000 ; ++k)); do
+    echo $k
+    pypy3 gen.py > gen.in
+    a.out < gen.in > fast.out \
+        # 2> /dev/null
+    b.out < gen.in > brute.out \
+        # 2> /dev/null
+    diff -w fast.out brute.out
+done
