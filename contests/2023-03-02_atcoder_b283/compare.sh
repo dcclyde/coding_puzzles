a61aa7f0a3907164838c9a4a87494fc4a3adfa58
David Clyde <dcclyde@gmail.com>
2023-03-02 21:58:44 -0800

    2023-03-02 atcoder b283

---
+++ contests/2023-03-02_atcoder_b283/compare.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+set -e
+
+PROBLEM="F"
+
+# any commands needed to prepare the solutions.
+# just comment out if one/both uses Python or otherwise doesn't need prep.
+g++ --std=c++2a "$PROBLEM".cpp -g -O2 -o a.out \
+    -DDCCLYDE_LOCAL
+g++ --std=c++2a -DDCCLYDE_BRUTEFORCE "$PROBLEM".cpp -g -O2 -o b.out \
+    -DDCCLYDE_LOCAL
+
+for((k = 1; k < 100000 ; ++k)); do
+    echo $k
+    pypy3 gen.py > gen.in
+    a.out < gen.in > fast.out \
+        # 2> /dev/null
+    b.out < gen.in > brute.out \
+        # 2> /dev/null
+    # pypy3 brute.py < gen.in > brute.out \
+    #     # 2> /dev/null
+
+    diff -w fast.out brute.out
+done
