b6f9616c19fade08a37d56c4350985a8241d98a3
David Clyde <dcclyde@gmail.com>
2023-04-15 12:54:03 -0700

    2023-04-15 codejam farewell, round C

---
+++ contests/2023-04-15_codejam_2023rC/A_christine.py
@@ -0,0 +1,314 @@
+#region imports
+import copy
+from collections import defaultdict as dd
+from collections import deque
+import math
+import sys
+import os
+#endregion
+#region  @bootstrap for recursive functions
+# # TLDR:
+# # Python's recursion limit is 1000. Increasing the limit often causes MLE.
+# # Instead, use this decorator. See https://codeforces.com/blog/entry/80158?#comment-662204.
+# # Usage instructions:
+# #   1) Put @bootstrap 1 line before the recursive function.
+# #   2) Replace "return" with "yield"
+# #   3) You also need to wrap your recursive calls like (yield myfunction(x)).
+# #          (applies even if the function doesn't return anything!)
+#            (only do this for recursive calls within the recursive function though!)
+# # Example (original version):
+# def f(n):
+#     if n <= 0: yield 1
+#     return f(n-1) + f(n-2)
+#
+# print(f(5))
+#
+# # Example (fixed):
+# @bootstrap
+# def f(n):
+#     if n <= 0: return 1
+#     yield (yield f(n-1)) + (yield f(n-2))
+#
+# print(f(5))  # don't add `yield` to the call from outside!
+#
+# Extended example:
+#     Before: https://pastebin.com/inwciZgh
+#     After: https://pastebin.com/fKksXXLj
+#     Try submitting at: https://codingcompetitions.withgoogle.com/codejam/round/0000000000876ff1/0000000000a45ef7
+
+from types import GeneratorType
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+#endregion
+#region  dbg commands
+# remember .bashrc should contain `export PYTHON_CONTEST_HELPER="dummy"`
+
+local_run = False
+if os.environ.get("PYTHON_CONTEST_HELPER"):
+    local_run = True
+
+    # call like dbg(print_details_helper(stuff))
+    # provides more detailed listing of complicated objects
+    def print_details_helper(q):
+        out = []
+        for k, x in enumerate(q):
+            out.append(f"\n\t{k}:\t{x}")
+        return ''.join(out) + '\n'
+
+    pdh = print_details_helper  # alternate shorter name
+
+    # good if we want to see spacing, or to copy-paste into GSheets
+    def print_tsv_helper(q):
+        out = []
+        for row in q:
+            out.append('\t'.join(str(x) for x in row))
+        return ''.join('\n\t' + x for x in out) + '\n'
+
+    pth = print_tsv_helper
+
+    OUT_RED_BOLD = "\033[31;1m"
+    OUT_GREEN = "\033[32m"
+    OUT_RESET = "\033[0m"
+    OUT_BOLD = "\033[;1m"
+    OUT_RED = "\033[31m"
+    OUT_WHITE = "\033[97m"
+    OUT_BLUE = "\033[34;1m"
+    OUT_CYAN = "\033[36;1m"
+    OUT_PURPLE = "\033[35;1m"
+    OUT_YELLOW = "\033[33;1m"
+    OUT_BACKGROUND = "\033[41;30m"
+
+    # helper function so your error printouts won't show up above an earlier stdout line.
+    def flush_stdout():
+        print('', flush=True, end='')
+
+    def dbgBase(*args, **kwargs):
+        flush_stdout()
+        color_helper = kwargs.pop('color', OUT_CYAN)
+        if kwargs.get('comment_first', False):
+            print(f'{OUT_BOLD}{color_helper}{args[0]: >11} {OUT_RESET}', end='', file=sys.stderr)
+            args = tuple(args[1:])
+        else:
+            print(f"{'': >12}", end='', file=sys.stderr)
+        if 'comment_first' in kwargs:
+            del kwargs['comment_first']
+        print(f"{OUT_RED_BOLD}{sys._getframe().f_back.f_back.f_lineno: >7} {OUT_BOLD}:  {OUT_RESET}{color_helper}", end='', file=sys.stderr)
+        end_maybe = kwargs.get('end', '\n')
+        kwargs['end']=f"{OUT_RESET}{end_maybe}"
+        if 'sep' not in kwargs:
+            kwargs['sep'] = ' '*3
+        kwargs['flush'] = True
+        print(*args, file=sys.stderr, **kwargs)
+
+    # Can always comment out some of these lines to disable those logs.
+    # e.g. if you make lots of messy printouts while finding bug #1 but now it's fixed but you still want SOME printouts.
+    def dbg(*args, **kwargs): dbgBase(color=OUT_CYAN, *args, **kwargs)
+    def dbgG(*args, **kwargs): dbgBase(color=OUT_GREEN, *args, **kwargs)
+    def dbgP(*args, **kwargs): dbgBase(color=OUT_PURPLE, *args, **kwargs)
+    def dbgY(*args, **kwargs): dbgBase(color=OUT_YELLOW, *args, **kwargs)
+    def dbgR(*args, **kwargs): dbgBase(color=OUT_RED, *args, **kwargs)
+    def dbgB(*args, **kwargs): dbgBase(color=OUT_BLUE, *args, **kwargs)
+    def dbgW(*args, **kwargs): dbgBase(color=OUT_WHITE, *args, **kwargs)
+    def dbgBackground(*args, **kwargs): dbgBase(color=OUT_BACKGROUND, *args, **kwargs)
+    def el(n=1): flush_stdout(); print('\n'*n, file=sys.stderr, end='', flush=True)
+    def dbgc(*args, **kwargs): dbgBase(comment_first=True, color=OUT_CYAN, *args, **kwargs)
+    def dbgcG(*args, **kwargs): dbgBase(comment_first=True, color=OUT_GREEN, *args, **kwargs)
+    def dbgcP(*args, **kwargs): dbgBase(comment_first=True, color=OUT_PURPLE, *args, **kwargs)
+    def dbgcY(*args, **kwargs): dbgBase(comment_first=True, color=OUT_YELLOW, *args, **kwargs)
+    def dbgcR(*args, **kwargs): dbgBase(comment_first=True, color=OUT_RED, *args, **kwargs)
+    def dbgcB(*args, **kwargs): dbgBase(comment_first=True, color=OUT_BLUE, *args, **kwargs)
+    def dbgcW(*args, **kwargs): dbgBase(comment_first=True, color=OUT_WHITE, *args, **kwargs)
+    def dbgcBackground(*args, **kwargs): dbgBase(comment_first=True, color=OUT_BACKGROUND, *args, **kwargs)
+else:
+    def dbg(*args, **kwargs): pass
+    def dbgG(*args, **kwargs): pass
+    def dbgP(*args, **kwargs): pass
+    def dbgY(*args, **kwargs): pass
+    def dbgR(*args, **kwargs): pass
+    def dbgB(*args, **kwargs): pass
+    def dbgW(*args, **kwargs): pass
+    def dbgBackground(*args, **kwargs): pass
+    def dbgc(*args, **kwargs): pass
+    def dbgcG(*args, **kwargs): pass
+    def dbgcP(*args, **kwargs): pass
+    def dbgcY(*args, **kwargs): pass
+    def dbgcR(*args, **kwargs): pass
+    def dbgcB(*args, **kwargs): pass
+    def dbgcW(*args, **kwargs): pass
+    def dbgcBackground(*args, **kwargs): pass
+    def el(n=1): pass
+    def print_details_helper(*args, **kwargs): pass
+    def print_tsv_helper(*args, **kwargs): pass
+    def pdh(*args, **kwargs): pass
+    def pth(*args, **kwargs): pass
+#endregion
+#region  FastIO
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+#endregion
+#region  input helpers
+def nn():
+    return int(input())
+
+def li():
+    return list(input())
+
+def lm():
+    return list(map(int, input().split()))
+
+# To print while flushing output for interactive problems:
+# print(x, flush=True)
+#endregion
+
+#########################################################################
+# Problem specific code usually goes below this line.
+#########################################################################
+
+
+def solve(testID):
+    N = nn()
+    dat = input().split()
+    dbg(N, dat)
+    result = []
+    # first entry in result is the first string in dat sorted in alphabetical order
+    result.append(sorted([x for x in dat[0]]))
+    for i in range(1, N):
+        sorted_word = sorted([x for x in dat[i]])
+        dbgG(sorted_word)
+        word_map = dd(int)
+        for c in sorted_word:
+            word_map[c] += 1
+        result_word_map = dd(int)
+        dbgB(result, i - 1)
+        for c in result[i-1]:
+            result_word_map[c] += 1
+        # check if the result_word_map is a subset of word_map
+        is_subset = True
+        for c in result_word_map:
+            if result_word_map[c] > word_map[c]:
+                is_subset = False
+                break
+        if is_subset:
+            result.append([x for x in result[i-1]])
+            for c in result_word_map:
+                word_map[c] -= result_word_map[c]
+            for c in sorted_word:
+                if word_map[c] > 0:
+                    result[-1].append(c)
+                    word_map[c] -= 1
+            continue
+        max_char = sorted_word[-1]
+        max_char_idx = 0
+        best_win_idx = -1
+        for j in range(len(result[i-1])):
+            x = result[i-1][j]
+            if x < max_char:
+                best_win_idx = j
+            if word_map[x] > 0:
+                word_map[x] -= 1
+                if x == max_char and word_map[x] == 0:
+                    for k in range(max_char_idx, len(sorted_word)):
+                        if word_map[sorted_word[-(k + 1)]] > 0:
+                            max_char = sorted_word[-(k + 1)]
+                            max_char_idx = k
+                            break
+            else:
+                break
+        dbgY(best_win_idx)
+        if best_win_idx == -1:
+            print(f"Case #{testID}: IMPOSSIBLE")
+            return
+        winning_word = []
+        word_map_2 = dd(int)
+        for c in sorted_word:
+            word_map_2[c] += 1
+        for j in range(best_win_idx):
+            winning_word.append(result[i-1][j])
+            word_map_2[result[i-1][j]] -= 1
+        for j in range(len(sorted_word)):
+            if sorted_word[j] > result[i - 1][best_win_idx] and word_map_2[sorted_word[j]] > 0:
+                winning_word.append(sorted_word[j])
+                word_map_2[sorted_word[j]] -= 1
+                break
+        for j in range(len(sorted_word)):
+            if word_map_2[sorted_word[j]] > 0:
+                winning_word.append(sorted_word[j])
+                word_map_2[sorted_word[j]] -= 1
+        result.append(winning_word)
+
+    print(f"Case #{testID}: POSSIBLE")
+    print(" ".join("".join(x) for x in result))
+    return
+
+
+if __name__ == '__main__':
+    T = 1
+    dbgBackground("Loading num cases!!!!!"); T = nn()  # ! Comment this out for single-case problems!
+    for testID in range(1, T+1):
+        el()
+        dbgBackground(f"Case {testID}")
+        solve(testID)
---
+++ contests/2023-04-15_codejam_2023rC/gen.py
@@ -0,0 +1,130 @@
+#region  load template's helpers
+import sys
+sys.path.append("/home/dcclyde/puzzles/code/templates")
+from template import *
+#endregion
+#region  ri, rv
+def ri(q, b=None):
+    if isinstance(b, int):
+        q = (q, b)
+    elif isinstance(q, int):
+        q = (q,q)
+    return random.randint(q[0], q[1])
+
+def rv(q, n):
+    N = ri(n)
+    return [ri(q) for _ in range(N)]
+#endregion
+#region  rgraph
+def rgraph(nr, er, mute=False):
+    N = ri(nr)
+    E = ri(er)
+    E = min(E, N*(N-1)//2)
+    edge_options = [(a, b) for a in range(N) for b in range(a+1, N)]
+    edges = random.sample(edge_options, E)
+
+    if not mute:
+        print(N, E)
+        pvn1(edges)
+    return N, E, edges
+#endregion
+#region  rtree
+#region  DSU
+class DSU:
+    def __init__(self, _N):
+        self.N = _N
+        self.e = [-1] * self.N
+    def get(self, x):
+        if self.e[x] < 0:
+            return x
+        self.e[x] = self.e[self.e[x]]
+        return self.e[x]
+    def sameSet(self, a,b): return self.get(a) == self.get(b)
+    def size(self, x): return -self.e[self.get(x)]
+    def unite(self, x, y):
+        x = self.get(x); y = self.get(y)
+        if (x == y): return False
+        if (self.e[x] > self.e[y]): x,y = y,x
+        self.e[x] += self.e[y]
+        self.e[y] = x
+        return True
+#endregion
+def rtree(nr, mute=False):
+    N = ri(nr)
+    edges = []
+    for a in range(1, N):
+        b = ri((0, a-1))
+        edges.append([a,b])
+        random.shuffle(edges[-1])
+
+    perm = list(range(N))
+    random.shuffle(perm)
+    edges = [[perm[x] for x in q] for q in edges]
+    if not mute:
+        print(N)
+        pvn1(edges)
+    return N, edges
+#endregion
+#region  rtree_rooted
+def rtree_rooted(nr, mute=False):
+    N, edges = rtree(nr, mute=True)
+    G = [[] for _ in range(N)]
+    for a, b in edges:
+        G[a].append(b)
+        G[b].append(a)
+    root = 0
+    parents = [-2] * N
+    parents[root] = -1
+    # traverse.
+    todo = [root]
+    while len(todo) > 0:
+        curr = todo.pop()
+        for o in G[curr]:
+            if parents[o] == -2:
+                parents[o] = curr
+                todo.append(o)
+
+    parents = parents[1:]
+    if not mute:
+        print(N)
+        pv1(parents)
+    return N, parents
+
+def rtree_rooted_v2(nr, mute=False):
+    # this version assumes v's parent will have an index < v.
+    N = ri(nr)
+    parents = [ri((0, k)) for k in range(N-1)]
+    if not mute:
+        print(N)
+        pv1(parents)
+    return N, parents
+#endregion
+
+
+N_RANGE = (3, 3)
+LEN_RANGE = (1, 5)
+LETTER_RANGE = (0, 25)
+LETTER_RANGE = (0, 3)
+
+def gen_test():
+    N = ri(N_RANGE)
+    dat = [
+        ''.join(chr(ord('A')+ri(LETTER_RANGE)) for _ in range(ri(LEN_RANGE)))
+        for __ in range(N)
+    ]
+    ps(1)  # ! uncomment for multi-case
+    ps(N)
+    pv(dat)
+
+
+# ! IF RANDOM TESTS PASS:
+# !   * OVERFLOW! Remember pii, FOR, maxi/mini, INF Write an "overflow gen"?
+# !   * RECODE FROM SCRATCH? Maybe in Python?
+# !   * Misunderstanding the problem rules?
+# !   * Brute force wrong - is it sufficiently different?
+# !   * Edge case not buildable in brute forceable inputs (or needs mid size AND pathological)
+#region  past mistakes
+# * Uninitialized variable
+# * I used INF = 1e9 but answer could be bigger
+#endregion
+gen_test()
