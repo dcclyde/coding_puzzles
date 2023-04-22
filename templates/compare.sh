6bc8be676effc4f799243ff9ee947e030477fe18
David Clyde <dcclyde@gmail.com>
2023-04-21 20:53:05 -0700

    compare.sh minor

--- templates/compare.sh
+++ templates/compare.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-set -e
+set -e  # Halt on error throughout this script (e.g. if diff finds a difference)
 
 if [ $# -ne 1 ]
 then
@@ -10,7 +10,7 @@ fi
 PROBLEM="invalid"
 PROBLEM=$1
 
-# any commands needed to prepare the solutions.
+# commands needed to prepare the solutions.
 # just comment out if one/both uses Python or otherwise doesn't need prep.
 g++ --std=c++20 "$PROBLEM".cpp -g -O2 -o a.out \
     -DDCCLYDE_LOCAL
@@ -24,8 +24,6 @@ for((k = 1; k < 100000 ; ++k)); do
         # 2> /dev/null
     b.out < gen.in > brute.out \
         # 2> /dev/null
-    # pypy3 brute.py < gen.in > brute.out \
-    #     # 2> /dev/null
 
     diff -w fast.out brute.out
 done
